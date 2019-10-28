80aa6ecb36ae1900a419174773f8297b5c1216fd
Dude112113 <curtisdude12@gmail.com>
2019-10-28 13:14:25 +0000

    Added error type and positions Moved functions into `file` from `file:symbolize()`

--- selenScript/file.lua
+++ selenScript/file.lua
@@ -36,151 +36,175 @@ function file.new(filepath)
 end
 
 
-function file:symbolize()
-	self.symbolizeDiagnostics = {}
-
-	local function getSymbol(key, ast)
-		if ast.type == "block" and ast.symbols ~= nil then
-			for symbolKey, symbol in pairs(ast.symbols) do
-				if symbolKey.isEqualValue ~= nil then
-					if symbolKey:isEqualValue(key) then
-						return symbol
-					end
-				else
-					table.insert(self.symbolizeDiagnostics, {
-						msg="INTERNAL: file:symbolize().getSymbol(): type " .. tostring(symbolKey.type) .. " is missing isEqualValue()"
-					})
-				end
-			end
-		end
-		if ast.parent == nil then
-			return nil
-		end
-		return getSymbol(key, ast.parent)
-	end
-	local function getSymbolTable(ast)
-		if ast.type == "block" and ast.symbols ~= nil then
-			return ast.symbols
-		end
-		if ast.parent == nil then
-			return nil
-		end
-		return getSymbolTable(ast.parent)
-	end
-	local function createSymbol(key, ast, value)
-		local symbolTable
-		local symbol
-		if ast.scope == "local" then
-			symbolTable = getSymbolTable(ast)
-		elseif ast.scope == "global" then
-			symbolTable = self.program.globals
-		else
-			symbol = getSymbol(key, ast)
-			if symbol ~= nil then
-			elseif self.program.settings.defaultLocals then
-				symbolTable = getSymbolTable(ast)
-			else
-				symbolTable = self.program.globals
-			end
-		end
-
-		if symbol ~= nil then
-			-- it's already defined
-			symbol:addDeclaration(ast)
-		elseif symbolTable ~= nil then
-			-- no definition or symbol yet, so we need to create them
-			symbol = Symbol.new {
-				key=key,
-				value=value
-			}
-			symbolTable[key] = symbol
-		else
-			table.insert(self.symbolizeDiagnostics, {
-				msg="INTERNAL: createSymbol Wait What? how did we get here?"
-			})
-			return
-		end
-		return symbol
-	end
-	local function symbolize(ast)
-		if ast.type == "block" then
-			ast.symbols = ast.symbols or {}
-			for i, stmt in ipairs(ast) do
-				symbolize(stmt)
-			end
-		elseif ast.type == "do" then
-			symbolize(ast.block)
-		elseif ast.type == "anon_function" then
-			symbolize(ast.body)
-		elseif ast.type == "function" then
-			local symbol = createSymbol(ast.funcname, ast, helpers.deepCopy(ast.body))
-			if symbol ~= nil then
-				symbol:addDeclaration(ast)
-			end
-			symbolize(ast.body)
-		elseif ast.type == "funcbody" then
-			local block = ast.block
-			local symbols = {}
-			block.symbols = symbols
-
-			for _, arg in ipairs(ast.args) do
-				local symbol = createSymbol(arg.name, block)
-				if symbol ~= nil then
-					symbol:addDeclaration(ast)
-				end
-			end
-
-			symbolize(block)
-		elseif ast.type == "assign" then
-			for i, var in ipairs(ast.var_list) do
-				local value = (ast.expr_list ~= nil and ast.expr_list[i]) or nil
-
-				local symbolValue = nil
-				if value ~= nil then
-					symbolize(value)
-					symbolValue = helpers.deepCopy(value)
-				end
-				local symbol = createSymbol(var, ast, symbolValue)
-				if symbol ~= nil then
-					symbol:addDeclaration(ast)
-				end
-			end
-		elseif ast.type == "index" then
-			if ast.index == nil then
-				local symbol = getSymbol(ast.expr, ast)
-				if symbol == nil then
-					table.insert(self.symbolizeDiagnostics, {
-						msg="Undefined variable " .. ((ast.toString and ast:toString()) or ("<type: " .. tostring(ast.type) .. " is missing toString()>"))
-					})
-					-- even though it was undefined, we want to still be able to link this up with any potential future declations or references
-					symbol = createSymbol(ast.expr, ast)
-				end
-				if symbol ~= nil then
-					symbol:addReference(ast)
+function file:getSymbol(key, ast)
+	if ast.type == "block" and ast.symbols ~= nil then
+		for symbolKey, symbol in pairs(ast.symbols) do
+			if symbolKey.isEqualValue ~= nil then
+				if symbolKey:isEqualValue(key) then
+					return symbol
 				end
 			else
 				table.insert(self.symbolizeDiagnostics, {
-					msg="UNIMPLEMENTED: file:symbolize().symbolize() type 'index': index into table"
+					type="internal",
+					start=ast.start,
+					finish=ast.finish,
+					msg="INTERNAL: file:symbolize().getSymbol(): type " .. tostring(symbolKey.type) .. " is missing isEqualValue()"
 				})
-				return
 			end
-		elseif ast.type == "table" then
-			local symbols = {}
-			ast.symbols = symbols
-		elseif ast.type == "Int" or ast.type == "Float" or ast.type == "Hex" or ast.type == "String" or ast.type == "LongString" or ast.type == "nil" then
-			-- Nothing to do
-		elseif ast.type == "LongComment" or ast.type == "Comment" then
-			-- Comments will have special stuff later on, like emmy lua support
+		end
+	end
+	if ast.parent == nil then
+		return nil
+	end
+	return self:getSymbol(key, ast.parent)
+end
+function file:getSymbolTable(ast)
+	if ast.type == "block" and ast.symbols ~= nil then
+		return ast.symbols
+	end
+	if ast.parent == nil then
+		return nil
+	end
+	return self:getSymbolTable(ast.parent)
+end
+function file:createSymbol(key, ast, value)
+	local symbolTable
+	local symbol
+	if ast.scope == "local" then
+		symbolTable = self:getSymbolTable(ast)
+	elseif ast.scope == "global" then
+		symbolTable = self.program.globals
+	else
+		symbol = self:getSymbol(key, ast)
+		if symbol ~= nil then
+		elseif self.program.settings.defaultLocals then
+			symbolTable = self:getSymbolTable(ast)
+		else
+			symbolTable = self.program.globals
+		end
+	end
+
+	if symbol ~= nil then
+		-- it's already defined
+		symbol:addDeclaration(ast)
+	elseif symbolTable ~= nil then
+		-- no definition or symbol yet, so we need to create them
+		symbol = Symbol.new {
+			key=key,
+			value=value
+		}
+		symbolTable[key] = symbol
+	else
+		table.insert(self.symbolizeDiagnostics, {
+			type="internal",
+			start=ast.start,
+			finish=ast.finish,
+			msg="INTERNAL: createSymbol Wait What? how did we get here?"
+		})
+		return
+	end
+	return symbol
+end
+function file:symbolizeAST(ast)
+	if ast.type == "block" then
+		ast.symbols = ast.symbols or {}
+		for i, stmt in ipairs(ast) do
+			self:symbolizeAST(stmt)
+		end
+	elseif ast.type == "do" then
+		self:symbolizeAST(ast.block)
+	elseif ast.type == "anon_function" then
+		self:symbolizeAST(ast.body)
+	elseif ast.type == "function" then
+		local symbol = self:createSymbol(ast.funcname, ast, helpers.deepCopy(ast.body))
+		if symbol ~= nil then
+			symbol:addDeclaration(ast)
+		end
+		self:symbolizeAST(ast.body)
+	elseif ast.type == "funcbody" then
+		local block = ast.block
+		local symbols = {}
+		block.symbols = symbols
+
+		for _, arg in ipairs(ast.args) do
+			local symbol = self:createSymbol(arg.name, block)
+			if symbol ~= nil then
+				symbol:addDeclaration(ast)
+			end
+		end
+
+		self:symbolizeAST(block)
+	elseif ast.type == "assign" then
+		for i, var in ipairs(ast.var_list) do
+			local value = (ast.expr_list ~= nil and ast.expr_list[i]) or nil
+
+			local symbolValue = nil
+			if value ~= nil then
+				self:symbolizeAST(value)
+				symbolValue = helpers.deepCopy(value)
+			end
+			local symbol = self:createSymbol(var, ast, symbolValue)
+			if symbol ~= nil then
+				symbol:addDeclaration(ast)
+			end
+		end
+	elseif ast.type == "index" then
+		if ast.index == nil then
+			local symbol = self:getSymbol(ast.expr, ast)
+			if symbol == nil then
+				table.insert(self.symbolizeDiagnostics, {
+					type="undefined",
+					start=ast.start,
+					finish=ast.finish,
+					msg="Undefined variable " .. ((ast.toString and ast:toString()) or ("<type: " .. tostring(ast.type) .. " is missing toString()>"))
+				})
+				-- even though it was undefined, we want to still be able to link this up with any potential future declations or references
+				symbol = self:createSymbol(ast.expr, ast)
+			end
+			if symbol ~= nil then
+				symbol:addReference(ast)
+			end
 		else
 			table.insert(self.symbolizeDiagnostics, {
-				msg="file:symbolize().symbolize() missing handle for type " .. helpers.strValue(ast.type)
+				type="internal",
+				start=ast.start,
+				finish=ast.finish,
+				msg="UNIMPLEMENTED: file:symbolizeAST() type 'index': index into table"
 			})
 			return
 		end
+	elseif ast.type == "table" then
+		local symbols = {}
+		ast.symbols = symbols
+	elseif ast.type == "Int" or ast.type == "Float" or ast.type == "Hex" or ast.type == "String" or ast.type == "LongString" or ast.type == "nil" then
+		-- Nothing to do
+	elseif ast.type == "LongComment" or ast.type == "Comment" then
+		-- Comments will have special stuff later on, like emmy lua support
+	else
+		table.insert(self.symbolizeDiagnostics, {
+			type="internal",
+			start=ast.start,
+			finish=ast.finish,
+			msg="INTERNAL: file:symbolizeAST() missing handle for type " .. helpers.strValue(ast.type)
+		})
+		return
 	end
+end
+function file:symbolize()
+	self.symbolizeDiagnostics = {}
 	local startTime = os.clock()
-	symbolize(self.ast)
+	self:symbolizeAST(self.ast)
 	return os.clock() - startTime
+end
+
+-- TODO
+function file:diagnose()
+	self.diagnostics = {}
+end
+
+-- TODO
+function file:transpile()
+	self.transpileDiagnostics = {}
 end
 
 
