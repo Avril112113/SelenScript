6ad9b331acb822aba72fe2c85c1c905955f85d1b
Dude112113 <curtisdude12@gmail.com>
2019-10-29 23:05:37 +0000

    Updated test.lua

--- test.lua
+++ test.lua
@@ -3,7 +3,8 @@ local ss = require "selenScript"
 
 local settings = {
 	defaultLocals=true,
-	indent="\t"
+	indent="\t",
+	targetVersion="5.1"
 }
 
 
@@ -16,6 +17,8 @@ print("--- Parse Errors ---")
 for _, err in ipairs(file.parseResult.errors) do
 	print(tostring(err.start) .. ":" .. tostring(err.finish) .. " " ..  err.msg)
 end
+-- print("--- AST ---")
+-- ss.helpers.printAST(file.ast)
 
 print()
 
@@ -50,6 +53,8 @@ local ok, transformer, transpiler, transformedAst = file:transpile()
 if not ok then
 	print("Failed to open file '" .. file:getWriteFilePath() .. "' to write")
 end
+-- print("--- AST (After Transform) ---")
+-- ss.helpers.printAST(transformedAst)
 print("--- Transformer Diagnostics ---")
 for _, err in ipairs(transformer.diagnostics) do
 	print(tostring(err.start) .. ":" .. tostring(err.finish) .. " " .. (err.severity or "unknown") .. ": " ..  err.msg)
