5bd6c039426ddca52ad492e6a99fc85b5e797313
Dude112113 <curtisdude12@gmail.com>
2022-10-23 15:56:18 +0100

    Removed `then` for conditional statements

--- tests/1_parser/3_selenscript/test_statements.lua
+++ tests/1_parser/3_selenscript/test_statements.lua
@@ -33,14 +33,10 @@ end)
 TestLib.test("conditional_stmt", function ()
 	local parser = TestLib.assert(ParserTestUtils.getTestParser())
 	local ast, errors, comments = ParserTestUtils.parse(parser, [[
-		if true then
-			continue
-		if true then
-			break
-		if true then
-			goto test
-		if true then
-			return
+		if true continue
+		if true break
+		if true goto test
+		if true return
 	]])
 	TestLib.assert(ast ~= nil, "ast ~= nil")
 	TestLib.assert(#errors <= 0, "#errors <= 0")
