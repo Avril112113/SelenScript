ad110b6a8be2366ce3f641ede35feb3b674cd56c
Avril112113 <curtisdude12@gmail.com>
2024-06-24 14:26:48 +0100

    Revert "Updated avcalcline"
    This reverts commit 800db56d38a4286632a5ca691dd7e93f291ee070.

--- libs/avcalcline.lua
+++ libs/avcalcline.lua
@@ -1,14 +1,8 @@
----@class AVCalcLine.Node
----@field pos integer
----@field line integer
----@field column integer
----@field left AVCalcLine.Node?
----@field right AVCalcLine.Node?
+---@alias AVCalcLine.Segment {pos:integer,line:integer,column:integer}
 
 ---@class AVCalcLine
 ---@field src string
----@field chunk_size integer
----@field tree AVCalcLine.Node
+---@field segments AVCalcLine.Segment[]
 local AVCalcLine = {}
 AVCalcLine.__index = AVCalcLine
 
@@ -17,50 +11,40 @@ AVCalcLine.__index = AVCalcLine
 function AVCalcLine.new(src)
 	local self = setmetatable({}, AVCalcLine)
 	self.src = src
-	self.tree = {pos=1,line=1,column=1}
+	self.segments = {}
+	local i, line, column = 1, 1, 1
+
+	while true do
+		table.insert(self.segments, {
+			pos=i,
+			line=line,
+			column=column,
+		})
+		local rest, nl_count = src:sub(i+1,i+1000):gsub("[^\n]*\n", "")
+		line = line + nl_count
+		column = #rest
+		column = column ~= 0 and column or 1
+		if i > #src then break end
+		i = i + 1000
+	end
 	return self
 end
 
 function AVCalcLine:calcline(pos)
-	if pos < 0 then
-		return 0, 0
-	end
-
-	local node = self.tree
-	while true do
-		if pos > node.pos then
-			if node.right then
-				node = node.right
-			else
-				break
-			end
-		elseif pos < node.pos then
-			if node.left then
-				node = node.left
-			else
-				break
-			end
-		else
+	local segment = self.segments[1]
+	local i = 2
+	while self.segments[i] ~= nil do
+		if self.segments[i].pos > pos then
 			break
+		else
+			segment = self.segments[i]
+			i = i + 1
 		end
 	end
-	---@cast node -?
-
-	if node.pos == pos then
-		return node.line, node.column
-	end
-
-	local rest, nl_count = self.src:sub(node.pos, pos):gsub("[^\n]*\n", "")
-	local line = node.line + nl_count
-	local column = (nl_count == 0 and node.column or 0) + #rest
+	local rest, nl_count = self.src:sub(segment.pos+1, pos):gsub("[^\n]*\n", "")
+	local line = segment.line + nl_count
+	local column = (nl_count == 0 and segment.column or 0) + #rest
 	column = column ~= 0 and column or 1
-
-	if pos > node.pos then
-		node.right = {pos=pos, line=line, column=column}
-	elseif pos < node.pos then
-		node.left = {pos=pos, line=line, column=column}
-	end
-
 	return line, column
 end
 
