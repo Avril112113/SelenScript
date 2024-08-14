8800841f441d3718052cc6ccabf3b6562d986fb3
Avril112113 <curtisdude12@gmail.com>
2024-08-15 00:45:56 +0100

    Added CBOR compatibility

--- SelenScript/parser/parser.lua
+++ SelenScript/parser/parser.lua
@@ -1,5 +1,6 @@
 local re = require "drelabel"
 local AVCalcLine = require "avcalcline"
+local CBOR = require "cbor"
 
 local Utils = require "SelenScript.utils"
 local Grammar = require "SelenScript.parser.grammar"
@@ -28,7 +29,7 @@ Parser.__index = Parser
 ---@param pos integer
 ---@return integer, integer
 function Parser._source_calcline(self, pos)
-	if self._avcalcline == nil then
+	if self._avcalcline == nil or self._avcalcline == CBOR.null then
 		self._avcalcline = AVCalcLine.new(self.source)
 	end
 	return self._avcalcline:calcline(pos)
@@ -101,6 +102,27 @@ function Parser:parse(source, file)
 		calcline = Parser._source_calcline
 	}
 	return ast_source, self.ast_defs.errors, self.ast_defs.comments
+end
+
+
+-- CBOR compatibility
+
+---@diagnostic disable-next-line: inject-field
+AVCalcLine.__tocbor = function()
+	return CBOR.encode(nil)
+end
+
+---@diagnostic disable-next-line: duplicate-set-field
+CBOR.type_encoders["function"] = function(f)
+	if f == Parser._source_calcline then
+		return CBOR.encode(CBOR.tagged(79135, CBOR.null))
+	end
+	local info = debug.getinfo(f, "S")
+	error(("can't encode function\ndefined at line %s in '%s'"):format(info.linedefined, info.source))
+end
+
+CBOR.tagged_decoders[79135] = function(...)
+	return Parser._source_calcline
 end
 
 
